ca3ee76084f617ddd250f7e867832be10db99432
yaroslav-demchenko <dastex1411@gmail.com>
2026-04-03 14:01:40 +0300

    indexes creation

--- task.sql
+++ task.sql
@@ -1,2 +1,5 @@
-# Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
+
+CREATE INDEX idx_customer_email ON Customers (Email); 
+
+CREATE INDEX idx_product_name ON Products (Name);
